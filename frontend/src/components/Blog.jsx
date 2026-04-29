f2f639a27cfe7c924220cc4dd403741859af497c
fabiopegini <fipegini@gmail.com>
2026-04-29 20:11:03 +0200

    Solution for Exercises 5.6 (previously done) & 5.7

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,7 +1,20 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [isExtended, setIsExtended] = useState(false)
+
+  return (
+    <div className="blog">
+      <div>
+        {blog.title} - {blog.author} 
+        <button type="button" onClick={() => setIsExtended(!isExtended)}>{isExtended ? 'Hide' : 'View'}</button>
+      </div>
+      {isExtended && <div>
+        <div>{blog.url}</div>
+        <div>Likes: {blog.likes} <button type="button">Like</button></div>
+      </div>}
+    </div>
+  )
+}
 
 export default Blog
